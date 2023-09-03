eab9b1f637a4de18affab8e6ad84ba36dbd840c3
Mohitpra7315 <MohitPrajapati7315@gmail.com>
2023-09-03 17:05:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { countrydata } from "./data"
+import SideNavbar from './Components/sideNavbar';
+import Newscont from './Components/NewsCont';
+import { useEffect, useState } from 'react';
+
+
+
+const NewsAPI = "https://newsapi.org/v2/top-headlines?country=hu&apiKey=029e54450e4540c39351a30c01b18aff";
 function App() {
+  const countryIcon = countrydata;
+
+  const [catagry, setCatagry] = useState('ar');
+
+  const [contrynews, setContrynews] = useState();
+
+  console.log(contrynews);
+  useEffect(() => {
+
+
+    async function Newsdata() {
+      try {
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${catagry}&apiKey=029e54450e4540c39351a30c01b18aff`)
+        const output = await response.json();
+        // console.log(output);
+        setContrynews(output)
+      } catch (error) {
+        console.log("dekho bhayia error aaya h ")
+      }
+    }
+
+    Newsdata();
+
+  }, [catagry])
+
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log(countryIcon)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">All OVER World News</h1>
+      <div>
+        cbind
+      </div>
+
+      <div className='container'>
+        {/* <sideNavbar countrydata={countryIcon} ></SideNavbar> */}
+        <SideNavbar countrydata={countryIcon} catagry={catagry} setCatagry={setCatagry}></SideNavbar>
+        <Newscont contrynews={contrynews} setContrynews={setContrynews}  ></Newscont>
+      </div>
+
     </div>
   );
 }
